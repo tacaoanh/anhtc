61d61def9c269de9340ab6588ae753b612ae2075
tacaoanh <giaitricuccu@gmail.com>
2016-07-28 10:02:01 +0700

    install project

---
+++ Destop.Pasgo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Destop.Pasgo.MvcApplication" Language="C#" %>
---
+++ Destop.Pasgo/Scripts/sukien.js
@@ -0,0 +1,27 @@
+﻿//sự kiện form contact
+$(document).ready(function () {
+    $('#rd2').hide();
+    $('#rd1').hide();
+    $('#ckbox').hide(300);
+    $('#form1').click(function () {
+        if ($('#rdkhachhang:checked').val() != true) {
+            $('#rd2').fadeIn(1000);
+        }
+        else {
+            $('#rd2').hide();
+            $('#rd1').hide();
+        }
+    });
+    $('#form2').click(function () {
+        if ($('#rdtuvan:checked').val() != true) {
+            console.log("rd:" + $('#rdtuvan:checked').val());
+            $('#rd1').fadeIn(1000);
+            $('#ckbox').fadeIn(1000);
+            $('#ck-online1').prop('checked',true);
+        }
+        else{
+            $('#rd1').hide();
+            
+        }
+    })
+});
